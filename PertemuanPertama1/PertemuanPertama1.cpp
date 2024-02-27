7f3adf8f39cf701b1c50a8d3e12bb426f54cec8c
SandyArdiyan <sanardiyan881@gmail.com>
2024-02-27 21:52:02 +0700

    membuat header

---
+++ PertemuanPertama1/PertemuanPertama1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float nJejari, nLuas;
+	cout << "=====Rumus Luas Lingkaran====="  << endl;
+	cout << "Masukan jejari= ";
+	cin >> nJejari;
+	nLuas = 3.14 * nJejari * nJejari;
+	cout << "Luasnya= " << nLuas;
+}
+
